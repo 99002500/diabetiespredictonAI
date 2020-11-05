8b8ba34ad05037b49ba9480083bdcc5ccb59e5e2
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-05 18:20:55 +0530

    started learning algo and libraries

---
+++ diabetes.py
@@ -0,0 +1,9 @@
+# This program detects if someone has diabeties or not
+
+# import the libraries
+
+import pandas as pd
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+ 
