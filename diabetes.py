0217cd22731582dab99d1bf96bec1de0874c6371
99002500 <sampreeth.rayadurga@ltts.com>
2020-11-06 14:35:59 +0530

    dgsg

--- diabetes.py
+++ diabetes.py
@@ -6,4 +6,13 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
- 
+from PIL import Image
+import streamlit as st
+
+# Create a title and a subtitle
+st.write("""
+# Diabetes detection
+Detect if someone has diabetes using machine learning and python
+""")
+# Open and display an image
+image = Image.open("diabetes.png")
